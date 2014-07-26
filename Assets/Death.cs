a07843564d0a9856c376ad0b04269f9b52027e66
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-07-26 12:30:26 -0400

    Switch Death from just using a RigidBody for movement to using a CharacterController. Also, move code for dealing with animation into Shared.cs rather than having an component that deals with it.

---
+++ Assets/Death.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class Death : MonoBehaviour {
+
+	public GameObject waypointsParent;
+	public GameObject player;
+	public CharacterController controller;
+
+	public float walkSpeed;
+	public float runSpeed;
+	public float proximityDistance;
+
+	private Path path;
+	private int pathIndex;
+
+	private Animator animator;
+
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		animator = GetComponentInChildren<Animator> ();
+
+		Seeker seeker = GetComponent<Seeker>();
+		seeker.StartPath (transform.position, player.transform.position, OnPathComplete);
+	}
+
+	void OnPathComplete (Path p) {
+		if (!p.error) {
+			path = p;
+			pathIndex = 0;
+		} else {
+			Debug.LogError("Couldn't find path: " + path);
+		}
+	}
+
+	Vector3 UpdateDir () {
+		if (path == null) {	return Vector3.zero; }
+		if (pathIndex >= path.vectorPath.Count) { return Vector3.zero; }
+		
+		Vector3 current = transform.position;
+		Vector3 goal = path.vectorPath [pathIndex];
+		goal.y = current.y;
+		
+		Vector3 dir = (goal - current).normalized;
+		dir *= walkSpeed;
+
+		if (Vector3.Distance (current, goal) < proximityDistance) {
+			pathIndex ++;
+		}
+
+		return dir;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = UpdateDir ();
+		controller.SimpleMove (dir);
+		
+		Shared.UpdateAnimator (animator, dir);
+	}
+}
---
+++ Assets/Shared.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shared {
+	public static readonly Vector3 forward = new Vector3(1,0,1);
+	public static readonly Vector3 right = new Vector3(1,0,-1);
+	
+	public static void UpdateAnimator (Animator animator, Vector3 dir) {
+		animator.SetFloat ("Speed", dir.magnitude);
+		
+		if (dir.magnitude != 0) {
+			if (Vector3.Dot (dir, right) > 0) {
+				animator.transform.localScale = new Vector3 (1, 1, 1);
+			} else {
+				animator.transform.localScale = new Vector3 (-1, 1, 1);
+			}
+		}
+	}
+
+}
